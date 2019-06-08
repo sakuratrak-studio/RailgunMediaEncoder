5e750201c8b536b6038b4e9bb1ecc6331444298e
EdgeNeko <EdgeNeko@aiursoft.com>
2019-06-08 12:13:30 +0800

    feat: add build config

--- ui/src/app/Components/home.component.ts
+++ ui/src/app/Components/home.component.ts
@@ -5,7 +5,12 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['../Styles/home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  constructor() {
+
+  }
   ngOnInit(): void {
   }
 
+  public test() {
+  }
 }
